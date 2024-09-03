f0b76e30f2a54ade1bd02f9eeda5275599c0daa0
Wong Tsang Han <tsanghan@gmail.com>
2024-09-03 21:55:53 +0800

    5th commit

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ exports.hello2 = async (event) => {
     })
   };
 };
+
+exports.hello3 = async (event) => {
+  console.log("*****HELLO-3*****")
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v4.0! Your function executed successfully! Function 3'
+    })
+  };
+};
